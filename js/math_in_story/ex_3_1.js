b6b5acb62e2080fb23344063243e8e63b451ab1d
sisackov <1123889+sisackov@users.noreply.github.com>
2021-11-11 16:57:52 +0200

    3.1 start

---
+++ js/math_in_story/ex_3_1.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @param {positive integer} p0 initial population
+ * @param {positive or null number} percent increase every year
+ * @param {integer} aug inhabitants coming or leaving each year
+ * @param {positive integer} p population to surpass
+ * @return n number of entire years needed to get a population greater or equal to p
+ */
+function nb_year(p0, percent, aug, p) {
+
+}
+
+console.log(nb_year(1500, 5, 100, 5000));//15
+console.log(nb_year(1500000, 2.5, 10000, 2000000));//10
