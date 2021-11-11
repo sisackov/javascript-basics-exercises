4e9ec69f9479500ac7c4f74b1592439a32f3ae72
sisackov <1123889+sisackov@users.noreply.github.com>
2021-11-11 16:22:43 +0200

    2.5 done

---
+++ js/simple_math/ex_2_5.js
@@ -0,0 +1,12 @@
+/**
+ * Using the summation formula :)
+ * @param {number} n integer greater than zero
+ * @returns sum of numbers of 1....n
+ */
+function summation(n) {
+    return (n * (n + 1)) / 2;
+}
+
+console.log(summation(2));//3
+console.log(summation(8));//36
+console.log(summation(7));//28
