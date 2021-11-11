c685321897b73d97e010138995a0b217d2109ada
sisackov <1123889+sisackov@users.noreply.github.com>
2021-11-11 16:41:43 +0200

    2.7 done

---
+++ js/simple_math/ex_2_7.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} operation 
+ * @param {number} value1
+ * @param {number} value2
+ * @returns result of the operation on the values
+ */
+function basicOp(operation, value1, value2) {
+    let result;
+    switch (operation) {
+        case '+':
+            result = value1 + value2;
+            break;
+        case '-':
+            result = value1 - value2;
+            break;
+        case '*':
+            result = value1 * value2;
+            break;
+        case '/':
+            result = value1 / value2;
+            break;
+    }
+    return result;
+}
+
+console.log(basicOp('+', 4, 7));  // Output: 11
+console.log(basicOp('-', 15, 18));// Output: -3
+console.log(basicOp('*', 5, 5));  // Output: 25
+console.log(basicOp('/', 49, 7)); // Output: 7
